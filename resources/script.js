6355ed5a0ffe1dd8b200c4532bdda530812018fd
Theorrax <106887196+Theorrax@users.noreply.github.com>
2023-11-01 01:41:34 -0400

    If applied, this commit will add a prompt to let the player select a choice of rock, paper, or scissors. Also, this commit will create a function that compares the computer choice and player choice and return a message with the respective result.

--- resources/script.js
+++ resources/script.js
@@ -1,8 +1,33 @@
-const allChoices = ["Rock", "Paper", "Scissors"];
+const allChoices = ["ROCK", "PAPER", "SCISSORS"];
 
 function getComputerChoice(){
     return allChoices[(Math.floor(Math.random() * allChoices.length))];
 }
 
-console.log(getComputerChoice());
-//Line 9 is a test 
+var playerEntry = prompt("Choose your hand: Rock, Paper or Scissors").toUpperCase()
+let playerScore = 0;
+let computerScore = 0;
+
+function playRound (computerSelection, playerSelection){
+    var computerSelection = getComputerChoice();
+    var playerSelection = playerEntry;
+    if (playerSelection === "ROCK" && computerSelection === "SCISSORS") {
+        playerScore += 1;
+        return "You win Rock smashes Scissors!";
+    } else if (playerSelection === "PAPER" && computerSelection === "ROCK") {
+        playerScore += 1;
+        return "You win Paper suffocates Rock!";
+    } else if (playerSelection === "SCISSORS" && computerSelection === "PAPER") {
+        playerScore += 1;
+        return "You're sharp, Scissors beats Paper!";
+    } else if (playerSelection === computerSelection) {
+        return "It's a tie try again!"
+    } else {
+        computerScore +=1;
+        return "You lose, try again!";
+    }
+}
+
+console.log(playRound());
+
+//TO DO: Make a loop until the computer or the player gets to five.
