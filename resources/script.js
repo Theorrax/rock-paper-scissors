6eb528d0663c1b809c813779ca6123b53fe46660
Theorrax <106887196+Theorrax@users.noreply.github.com>
2024-03-05 18:59:12 -0500

    If applied, this commit will create a function which generates a random selection of rock, paper, or scissors. The random selection is stored within a variable called computerChoice

--- resources/script.js
+++ resources/script.js
@@ -1,7 +1,17 @@
-//Variable that stores the choice of Rock, Paper, or Scissor.
 const allChoices = ["ROCK", "PAPER", "SCISSORS"]
+let computerChioce = generateComputerChoice();
 
 function getRandomNumber (min, max) {
   return Math.floor(Math.random() * (max - min + 1) ) + min; 
 }
 
+function generateComputerChoice () {
+    let number = getRandomNumber(0, 100)
+    if (number <= 33) {
+        return allChoices[0];
+    } else if (number >= 34 && number <= 66) {
+        return allChoices[1];
+    } else if (number >= 67) {
+        return allChoices[2];
+    }
+}
